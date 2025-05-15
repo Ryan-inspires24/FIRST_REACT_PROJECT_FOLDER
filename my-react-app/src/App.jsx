10950351f747575e42fd52151c9e9ee47a876136
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-05-15 11:50:59 +0100

    Used a josn object instead as my server, and updated my button to hide products once shown

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,28 +5,37 @@ import { useQuery } from '@tanstack/react-query';
 
 function App() {
   const [showProducts, setShowProducts] = useState(false);
+  const fetchProducts = async () =>{
+    const response = await fetch('products.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  }
   const { isloading, error, data} = useQuery({
     queryKey: ['products'],
-    queryFn: () => {
-      return fetch('http://localhost:5000/api/products')
-
-        .then((res) => res.json());  
-    },
+    queryFn: fetchProducts,
+    refetchOnWindowFocus: false,
     enabled: showProducts,
   });
 
     const handleShowProducts = () => {
-      setShowProducts(true);
+      setShowProducts((prev) => !prev);
+
     }
+    
 
       return (
         <>
           <h1>All Products</h1>
-<button onClick={handleShowProducts}>Show All Products</button>
-         {isloading && <h2>Loading Products...</h2>}
-         {error && <h2>Error fetching products</h2>}
-      {data && Array.isArray(data) ? (
-        <div>
+  <button className='togglebtn' onClick={handleShowProducts}>{showProducts ? 'Hide Products' : 'Show Products'} 
+</button>
+          {!showProducts && <h2 className='mssg'>No Products here</h2>}
+         {isloading && <h2 className='mssg'>Loading Products...</h2>}
+         {error && <h2 className='mssg'>Error fetching products</h2>}
+      {showProducts && data && Array.isArray(data) ? (
+        <div className='productsDiv'>
           {data.map((item) => (
             <DefaultItem key={item.id} item={item} />
           ))}
@@ -34,7 +43,6 @@ function App() {
       ) : (
         data && <h2>Unexpected data format</h2>
       )}
-                {data && !Array.isArray(data) && <h2>Unexpected data format</h2>}
 
         </>
 
--- my-react-app/src/components/lists/lists.jsx
+++ my-react-app/src/components/lists/lists.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './list.css'
 function DefaultItem(props) {
     console.log(props)
     const{id, name} = props.item
     return (
-        <div>
+        <div className='itemDiv'>
             <p>{id}. {name}</p>
         </div>
     )
